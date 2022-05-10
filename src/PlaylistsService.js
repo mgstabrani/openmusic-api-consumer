333e8fa5b9fa9e646c807655c7f383407bdbfd8b
Mgs. Tabrani <63233752+mgstabrani@users.noreply.github.com>
2022-05-10 23:34:47 +0700

    feature fix: result response (#2)
    * feature-fix: result response

--- src/PlaylistsService.js
+++ src/PlaylistsService.js
@@ -5,15 +5,28 @@ class PlaylistsService {
     this.pool = new Pool();
   }
 
-  async getPlaylists(userId) {
+  async getPlaylistById(id) {
     const query = {
-      text: `SELECT playlists.* FROM playlists
-      LEFT JOIN collaborations ON collaborations.playlist_id = playlists.id
-      WHERE playlists.owner = $1 OR collaborations.user_id = $1
-      GROUP BY playlists.id`,
-      values: [userId],
+      text: `SELECT playlists.id, playlists.name, users.username
+    FROM playlists
+    LEFT JOIN users ON users.id = playlists.owner
+    WHERE playlists.id = $1`,
+      values: [id],
     };
     const result = await this.pool.query(query);
+
+    return result.rows[0];
+  }
+
+  async getPlaylistSongs(playlistId) {
+    const query = {
+      text: `SELECT songs.id, songs.title, songs.performer
+      FROM playlists, songs, playlist_songs
+      WHERE playlists.id = $1 AND playlist_songs.song_id = songs.id AND playlists.id = playlist_songs.playlist_id`,
+      values: [playlistId],
+    };
+    const result = await this.pool.query(query);
+
     return result.rows;
   }
 }
